4274b8716aeb6962b8a58ba48bed892998d8df17
peter <wleowndud05@naver.com>
2018-06-23 23:46:42 +0900

    test2

--- myWeb/src/main/java/com/web/controller/MainController.java
+++ myWeb/src/main/java/com/web/controller/MainController.java
@@ -1,17 +1,24 @@
 package com.web.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 @RequestMapping(value = "/Main")
-public class WebController {
+public class MainController {
+	
+	private static final Logger logger =
+			LoggerFactory.getLogger(MainController.class);
 		
 	@RequestMapping(value ="/Main", method={RequestMethod.GET})
 	public String main(){
-		System.out.println("String" + 1);
-		return "";
+		
+		return "main/main";
 	}
 	
 	
